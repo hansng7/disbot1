ca2698dfcba541d2f90aba89b9f2db0d292d3124
Hans <30684540+hansng7@users.noreply.github.com>
2022-03-20 17:10:13 +0000

    Add $ generic command and  command

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
+import os
 import discord
+
+token = os.environ['TOKEN']
+reminders_channel_id = 854328114837585921
+test_channel_id = 955128791204765797
+travelers_role_id = 872748500033630218
+
+def get_rolemention_str(id):
+  return '<@&{0}>'.format(id)
+  
+def get_usermention_str(id):
+  return '<@!{0}>'.format(id)
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print('Logged in as {0}'.format(client.user))
+  await client.get_channel(test_channel_id).send('{0} entered chat'.format(client.user))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if (message.content == '$remind') and (message.author.guild_permissions.administrator):
+    await client.get_channel(reminders_channel_id).send('{0} Check in'.format(get_rolemention_str(travelers_role_id)))
+  
+  elif message.content.startswith('$') or (client.user in message.mentions):
+    await message.channel.send('Hello {0}'.format(message.author.mention)) 
+
+client.run(token, bot=True)
